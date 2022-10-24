0bdfd510cb0cd439bc5f3ff4b892e5f69580dd04
Rakibul2580 <rh565153@gmail.com>
2022-10-24 20:29:52 +0300

    all data setup

--- index.js
+++ index.js
@@ -3,13 +3,11 @@ const app = express();
 var cors = require("cors");
 const port = 5000;
 const name = require("./data/items-name.json");
+const items = require("./data/all-items.json");
 
 app.use(cors());
 app.get("/", (req, res) => {
   res.send(name);
-});
-app.get("/n", (req, res) => {
-  res.send({ name: "Rakibul Hasan" });
 });
 
 app.listen(port, () => {
@@ -17,7 +15,7 @@ app.listen(port, () => {
 });
 
 // git init
-// git add
+// git add .
 // git commit -m ""
 // git branch -M main
 // git remote add origin https://github.com/programming-hero-web-course1/b610-lerning-platform-server-side-Rakibul2580.git
